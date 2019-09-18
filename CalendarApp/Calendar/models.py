189dbf9993409ea5b65821128af799d62c2399d8
Johan Guzman <jguzm022@ucr.edu>
2019-09-18 10:34:26 -0700

    Fixing a comment

--- CalendarApp/Calendar/models.py
+++ CalendarApp/Calendar/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField # THIS LINE OF CODE GIVES A WEIRD ERROR
+from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
 from django.utils import timezone
 
