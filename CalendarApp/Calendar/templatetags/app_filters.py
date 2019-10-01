003cfc3ca68d1a4cef4c1b9f2b5edb01d923539b
Johan Guzman <jguzm022@ucr.edu>
2019-10-01 07:57:26 -0700

    Improving the display of hours and minutes in a summary, still needs to be further improved

--- CalendarApp/Calendar/templatetags/app_filters.py
+++ CalendarApp/Calendar/templatetags/app_filters.py
@@ -4,5 +4,9 @@ from datetime import date, timedelta
 register = template.Library()
 
 @register.filter(name='get_total_hours')
-def get_due_date_string(value):
-    return (value.days * 24) + (value.seconds / 3600)
+def get_due_hour_string(value):
+    return (value.days * 24) + (value.seconds // 3600)
+
+@register.filter(name='get_total_minutes')
+def get_due_min_string(value):
+   return (value.seconds % 3600) // 60
